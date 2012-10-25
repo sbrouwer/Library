321153006d81b2e83827d3c6053b2e971e1a05c9
Simon Brouwer <simon.brouwer@codemonkeys.ch>
2012-10-25 15:26:58 +0200

    Klasse TableModelBookDetail hinzugefügt(noch nichts implementiert)

---
+++ src/domain/TableModelBookDetail.java
@@ -0,0 +1,11 @@
+package domain;
+
+import javax.swing.table.DefaultTableModel;
+
+public class TableModelBookDetail extends DefaultTableModel {
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		return null;
+	}
+}
