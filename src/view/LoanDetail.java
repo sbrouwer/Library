0596ea71e968812598f82fa4561821fb30c0cd4e
Nurtak <Nurtak@152.96.236.48>
2012-11-20 15:53:54 +0100

    LoanDetail hinzugefügt

---
+++ src/view/LoanDetail.java
@@ -0,0 +1,235 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class LoanDetail
+{
+
+	private JFrame frmAusleiheDetail;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField txtAsdasd;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					LoanDetail window = new LoanDetail();
+					window.frmAusleiheDetail.setVisible(true);
+				} catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public LoanDetail()
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize()
+	{
+		frmAusleiheDetail = new JFrame();
+		frmAusleiheDetail.setTitle("Ausleihe Detail");
+		frmAusleiheDetail.setBounds(100, 100, 450, 300);
+		frmAusleiheDetail.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frmAusleiheDetail.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel customerPanel = new JPanel();
+		customerPanel.setBorder(new TitledBorder(null, "Kundenauswahl", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_customerPanel = new GridBagConstraints();
+		gbc_customerPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_customerPanel.fill = GridBagConstraints.BOTH;
+		gbc_customerPanel.gridx = 0;
+		gbc_customerPanel.gridy = 0;
+		frmAusleiheDetail.getContentPane().add(customerPanel, gbc_customerPanel);
+		GridBagLayout gbl_customerPanel = new GridBagLayout();
+		gbl_customerPanel.columnWidths = new int[]{100, 0, 0};
+		gbl_customerPanel.rowHeights = new int[]{0, 0, 0};
+		gbl_customerPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_customerPanel.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		customerPanel.setLayout(gbl_customerPanel);
+		
+		JLabel lblKennung = new JLabel("Kennung:");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		customerPanel.add(lblKennung, gbc_lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setText("1882");
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		customerPanel.add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblKunde = new JLabel("Kunde:");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		customerPanel.add(lblKunde, gbc_lblNewLabel_1);
+		
+		JComboBox comboBox = new JComboBox();
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 1;
+		customerPanel.add(comboBox, gbc_comboBox);
+		
+		JPanel newCopyPanel = new JPanel();
+		newCopyPanel.setBorder(new TitledBorder(null, "Neues Exemplar ausleihen", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_newCopyPanel = new GridBagConstraints();
+		gbc_newCopyPanel.insets = new Insets(0, 0, 5, 0);
+		gbc_newCopyPanel.fill = GridBagConstraints.BOTH;
+		gbc_newCopyPanel.gridx = 0;
+		gbc_newCopyPanel.gridy = 1;
+		frmAusleiheDetail.getContentPane().add(newCopyPanel, gbc_newCopyPanel);
+		GridBagLayout gbl_newCopyPanel = new GridBagLayout();
+		gbl_newCopyPanel.columnWidths = new int[]{100, 0, 0, 0, 0};
+		gbl_newCopyPanel.rowHeights = new int[]{0, 0, 0};
+		gbl_newCopyPanel.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_newCopyPanel.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		newCopyPanel.setLayout(gbl_newCopyPanel);
+		
+		JLabel lblExemplarID = new JLabel("Exemplar-ID:");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 0;
+		newCopyPanel.add(lblExemplarID, gbc_lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("123123");
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 0;
+		newCopyPanel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblX = new JLabel("X");
+		GridBagConstraints gbc_lblX = new GridBagConstraints();
+		gbc_lblX.insets = new Insets(0, 0, 5, 5);
+		gbc_lblX.gridx = 2;
+		gbc_lblX.gridy = 0;
+		newCopyPanel.add(lblX, gbc_lblX);
+		
+		JButton btnExemplarAusleihen = new JButton("Exemplar ausleihen");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.anchor = GridBagConstraints.WEST;
+		gbc_btnNewButton.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton.gridx = 3;
+		gbc_btnNewButton.gridy = 0;
+		newCopyPanel.add(btnExemplarAusleihen, gbc_btnNewButton);
+		
+		JLabel lblZurueckAm = new JLabel("Zur\u00FCck am:");
+		GridBagConstraints gbc_lblZurckAm = new GridBagConstraints();
+		gbc_lblZurckAm.anchor = GridBagConstraints.WEST;
+		gbc_lblZurckAm.insets = new Insets(0, 0, 0, 5);
+		gbc_lblZurckAm.gridx = 0;
+		gbc_lblZurckAm.gridy = 1;
+		newCopyPanel.add(lblZurueckAm, gbc_lblZurckAm);
+		
+		txtAsdasd = new JTextField();
+		txtAsdasd.setText("asdasd");
+		GridBagConstraints gbc_txtAsdasd = new GridBagConstraints();
+		gbc_txtAsdasd.gridwidth = 3;
+		gbc_txtAsdasd.insets = new Insets(0, 0, 0, 5);
+		gbc_txtAsdasd.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtAsdasd.gridx = 1;
+		gbc_txtAsdasd.gridy = 1;
+		newCopyPanel.add(txtAsdasd, gbc_txtAsdasd);
+		txtAsdasd.setColumns(10);
+		
+		JPanel loanByCustomerTablePanel = new JPanel();
+		loanByCustomerTablePanel.setBorder(new TitledBorder(null, "Ausleihen von VAR", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		GridBagConstraints gbc_loanByCustomerTablePanel = new GridBagConstraints();
+		gbc_loanByCustomerTablePanel.fill = GridBagConstraints.BOTH;
+		gbc_loanByCustomerTablePanel.gridx = 0;
+		gbc_loanByCustomerTablePanel.gridy = 2;
+		frmAusleiheDetail.getContentPane().add(loanByCustomerTablePanel, gbc_loanByCustomerTablePanel);
+		GridBagLayout gbl_loanByCustomerTablePanel = new GridBagLayout();
+		gbl_loanByCustomerTablePanel.columnWidths = new int[]{0, 0, 0};
+		gbl_loanByCustomerTablePanel.rowHeights = new int[]{0, 0, 0};
+		gbl_loanByCustomerTablePanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_loanByCustomerTablePanel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		loanByCustomerTablePanel.setLayout(gbl_loanByCustomerTablePanel);
+		
+		JLabel lblAnzahlAusleihen = new JLabel("Anzahl Ausleihen:");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 0;
+		loanByCustomerTablePanel.add(lblAnzahlAusleihen, gbc_lblNewLabel_3);
+		
+		JLabel lblAnzahlAusleihenAmount = new JLabel("2");
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.WEST;
+		gbc_label.insets = new Insets(0, 0, 5, 0);
+		gbc_label.gridx = 1;
+		gbc_label.gridy = 0;
+		loanByCustomerTablePanel.add(lblAnzahlAusleihenAmount, gbc_label);
+		
+		table = new JTable();
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.insets = new Insets(0, 0, 0, 5);
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 0;
+		gbc_table.gridy = 2;
+		loanByCustomerTablePanel.add(table, gbc_table);
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 2;
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		loanByCustomerTablePanel.add(scrollPane, gbc_scrollPane);
+		
+
+	}
+
+}
