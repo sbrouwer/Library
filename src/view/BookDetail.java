d959adafd894b2c10c2bd8f1787810f35d78f562
Simon Brouwer <simon.brouwer@codemonkeys.ch>
2012-11-29 15:46:57 +0100

    Escape Listener für BookDetail implementiert

--- src/view/BookDetail.java
+++ src/view/BookDetail.java
@@ -7,6 +7,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -18,12 +19,14 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.border.TitledBorder;
 
 import tablemodel.TableModelBookDetail;
@@ -76,6 +79,8 @@ public class BookDetail implements Observer
 		frame.setTitle("Buch Detail Ansicht");
 		Dimension d = new Dimension(450, 360);
 		frame.setMinimumSize(d);
+		
+		addKeyboardListeners(frame);
 
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(null, "Buch Informationen", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -297,6 +302,20 @@ public class BookDetail implements Observer
 			txtVerlag.setText(book.getPublisher());
 			regalComboBox.setSelectedItem((book.getShelf().toString()));
 		}
+	}
+	public void addKeyboardListeners(final JFrame frame) {
+	    ActionListener escListener = new ActionListener() {
+
+	        @Override
+	        public void actionPerformed(ActionEvent e) {
+	            frame.dispose();
+	        }
+	    };
+	    
+
+	    frame.getRootPane().registerKeyboardAction(escListener,
+	            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+	            JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
 
 	@Override
