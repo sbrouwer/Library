c2c4498d43d710bb6685a230cb9e63a843ef3450
Simon Brouwer <simon.brouwer@codemonkeys.ch>
2012-12-08 16:58:05 +0100

    TabCustomer enter listener auf Table hinzugefügt

--- src/view/TabCustomer.java
+++ src/view/TabCustomer.java
@@ -36,8 +36,7 @@ import domain.Book;
 import domain.Customer;
 import domain.Library;
 
-public class TabCustomer extends JPanel implements Observer
-{
+public class TabCustomer extends JPanel implements Observer {
 	private Library library;
 	private JTextField txtSearch;
 	private TableModelTabCustomer tableModel;
@@ -47,26 +46,25 @@ public class TabCustomer extends JPanel implements Observer
 	private JLabel lblAmountOfCustomers;
 	private HashMap<Customer, CustomerEdit> openOnce;
 
-	public TabCustomer(Library library)
-	{
+	public TabCustomer(Library library) {
 		this.library = library;
-	    library.addObserver(this);
-	    openOnce = new HashMap<Customer, CustomerEdit>();
+		library.addObserver(this);
+		openOnce = new HashMap<Customer, CustomerEdit>();
 		initialize();
-	    updateStatistics();
+		updateStatistics();
 	}
 
-	private void initialize()
-	{
+	private void initialize() {
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWidths = new int[] { 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 0, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
 		setLayout(gridBagLayout);
-		
+
 		JPanel panel_statistics = new JPanel();
-		panel_statistics.setBorder(new TitledBorder(null, "Statistiken", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_statistics.setBorder(new TitledBorder(null, "Statistiken", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
 		GridBagConstraints gbc_panel_statistics = new GridBagConstraints();
 		gbc_panel_statistics.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_statistics.fill = GridBagConstraints.BOTH;
@@ -74,26 +72,26 @@ public class TabCustomer extends JPanel implements Observer
 		gbc_panel_statistics.gridy = 0;
 		add(panel_statistics, gbc_panel_statistics);
 		GridBagLayout gbl_panel_statistics = new GridBagLayout();
-		gbl_panel_statistics.columnWidths = new int[]{0, 0, 0};
-		gbl_panel_statistics.rowHeights = new int[]{0, 0};
-		gbl_panel_statistics.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel_statistics.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		gbl_panel_statistics.columnWidths = new int[] { 0, 0, 0 };
+		gbl_panel_statistics.rowHeights = new int[] { 0, 0 };
+		gbl_panel_statistics.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panel_statistics.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		panel_statistics.setLayout(gbl_panel_statistics);
-		
+
 		JLabel lblCustomers = new JLabel("Anzahl Kunden:");
 		GridBagConstraints gbc_lblCustomers = new GridBagConstraints();
 		gbc_lblCustomers.insets = new Insets(0, 0, 0, 5);
 		gbc_lblCustomers.gridx = 0;
 		gbc_lblCustomers.gridy = 0;
 		panel_statistics.add(lblCustomers, gbc_lblCustomers);
-		
+
 		lblAmountOfCustomers = new JLabel();
 		GridBagConstraints gbc_lblAmountOfCustomers = new GridBagConstraints();
 		gbc_lblAmountOfCustomers.anchor = GridBagConstraints.WEST;
 		gbc_lblAmountOfCustomers.gridx = 1;
 		gbc_lblAmountOfCustomers.gridy = 0;
 		panel_statistics.add(lblAmountOfCustomers, gbc_lblAmountOfCustomers);
-		
+
 		JPanel panel_management = new JPanel();
 		GridBagConstraints gbc_panel_management = new GridBagConstraints();
 		gbc_panel_management.fill = GridBagConstraints.BOTH;
@@ -101,31 +99,28 @@ public class TabCustomer extends JPanel implements Observer
 		gbc_panel_management.gridy = 1;
 		add(panel_management, gbc_panel_management);
 		GridBagLayout gbl_panel_management = new GridBagLayout();
-		gbl_panel_management.columnWidths = new int[]{0};
-		gbl_panel_management.rowHeights = new int[]{0, 0};
-		gbl_panel_management.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panel_management.rowWeights = new double[]{0.0, 1.0};
+		gbl_panel_management.columnWidths = new int[] { 0 };
+		gbl_panel_management.rowHeights = new int[] { 0, 0 };
+		gbl_panel_management.columnWeights = new double[] { Double.MIN_VALUE };
+		gbl_panel_management.rowWeights = new double[] { 0.0, 1.0 };
 		panel_management.setLayout(gbl_panel_management);
-			
-		panel_management.setBorder(new TitledBorder(null, "Kundenverwaltung", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		
+
+		panel_management.setBorder(new TitledBorder(null, "Kundenverwaltung", TitledBorder.LEADING,
+				TitledBorder.TOP, null, null));
+
 		txtSearch = new JTextField();
 		txtSearch.setText("Suche");
-		txtSearch.addFocusListener(new FocusAdapter()
-		{
+		txtSearch.addFocusListener(new FocusAdapter() {
 			@Override
-			public void focusGained(FocusEvent arg0)
-			{
-				if (txtSearch.getText().contains("Suche"))
-				{
+			public void focusGained(FocusEvent arg0) {
+				if (txtSearch.getText().contains("Suche")) {
 					txtSearch.setText("");
 				}
 			}
+
 			@Override
-			public void focusLost(FocusEvent arg0)
-			{
-				if (txtSearch.getText().contains(""))
-				{
+			public void focusLost(FocusEvent arg0) {
+				if (txtSearch.getText().contains("")) {
 					txtSearch.setText("Suche");
 				}
 			}
@@ -143,24 +138,25 @@ public class TabCustomer extends JPanel implements Observer
 		gbc_textField_search.gridx = 0;
 		gbc_textField_search.gridy = 0;
 		panel_management.add(txtSearch, gbc_textField_search);
-		
-		ImageIcon iconCustomerEdit = new ImageIcon("icons/customer_edit.png");		
+
+		ImageIcon iconCustomerEdit = new ImageIcon("icons/customer_edit.png");
 		btnEditCustomer = new JButton("Kunde editieren", iconCustomerEdit);
-		btnEditCustomer.setToolTipText("Zeigt der in der untenstehende Tabelle ausgewählten Kunde zum editieren an");
+		btnEditCustomer
+				.setToolTipText("Zeigt der in der untenstehende Tabelle ausgewählten Kunde zum editieren an");
 		btnEditCustomer.setEnabled(false);
 		btnEditCustomer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			    openCustomerEditForAllSelectedCustomers();
+				openCustomerEditForAllSelectedCustomers();
 			}
 		});
-		
+
 		GridBagConstraints gbc_btnChangeSelectedCustomer = new GridBagConstraints();
 		gbc_btnChangeSelectedCustomer.insets = new Insets(0, 0, 5, 5);
 		gbc_btnChangeSelectedCustomer.gridx = 1;
 		gbc_btnChangeSelectedCustomer.gridy = 0;
 		panel_management.add(btnEditCustomer, gbc_btnChangeSelectedCustomer);
-		
-		ImageIcon iconCustomerAdd = new ImageIcon("icons/customer_add.png");		
+
+		ImageIcon iconCustomerAdd = new ImageIcon("icons/customer_add.png");
 		JButton btnNewCustomer = new JButton("Neuer Kunde", iconCustomerAdd);
 		btnNewCustomer.setToolTipText("Öffnet ein Fenster um einen neuen Kunden zu erfassen");
 		btnNewCustomer.addActionListener(new ActionListener() {
@@ -174,7 +170,7 @@ public class TabCustomer extends JPanel implements Observer
 		gbc_btnNewCustomer.gridx = 2;
 		gbc_btnNewCustomer.gridy = 0;
 		panel_management.add(btnNewCustomer, gbc_btnNewCustomer);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.gridwidth = 3;
@@ -182,10 +178,10 @@ public class TabCustomer extends JPanel implements Observer
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 1;
 		panel_management.add(scrollPane, gbc_scrollPane);
-		
+
 		table = new JTable();
 		table.getTableHeader().setReorderingAllowed(false);
-		table.addMouseListener(new MouseAdapter() {			
+		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				if (table.getSelectedRows().length > 0) {
@@ -193,19 +189,21 @@ public class TabCustomer extends JPanel implements Observer
 				} else {
 					btnEditCustomer.setEnabled(false);
 				}
-			}			
+			}
+
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				if(e.getClickCount() == 2){
-				    openCustomerEditForAllSelectedCustomers();
+				if (e.getClickCount() == 2) {
+					openCustomerEditForAllSelectedCustomers();
 				}
 			}
 		});
-		
+
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setAutoCreateRowSorter(true);
 
-		tableModel = new TableModelTabCustomer(library, new String[] { "Kunden-ID", "Name", "Vorname", "Strasse", "PLZ", "Ort" });
+		tableModel = new TableModelTabCustomer(library, new String[] { "Kunden-ID", "Name", "Vorname",
+				"Strasse", "PLZ", "Ort" });
 		table.setModel(tableModel);
 
 		table.getColumnModel().getColumn(0).setMinWidth(80);
@@ -213,15 +211,28 @@ public class TabCustomer extends JPanel implements Observer
 		table.getColumnModel().getColumn(4).setMinWidth(50);
 		table.getColumnModel().getColumn(4).setMaxWidth(50);
 
+		table.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				if (arg0.getKeyCode() == 10) {
+					if (table.getSelectedRow() != -1) {
+						table.setRowSelectionInterval(table.getSelectedRow() - 1, table.getSelectedRow() - 1);
+						openCustomerEditForAllSelectedCustomers();
+					}
+				}
+			}
+		});
+
 		scrollPane.setViewportView(table);
-		
+
 		sorter = new TableRowSorter<TableModelTabCustomer>(tableModel);
 		table.setRowSorter(sorter);
 		sorter.setSortsOnUpdates(true);
 		sorter.toggleSortOrder(1);
-	
-		}
-	
+
+	}
+
 	private void search() {
 		RowFilter<TableModelTabCustomer, Object> rf = null;
 		List<RowFilter<TableModelTabCustomer, Object>> filters = new ArrayList<RowFilter<TableModelTabCustomer, Object>>();
@@ -239,14 +250,14 @@ public class TabCustomer extends JPanel implements Observer
 					"(?i)^.*" + txtSearch.getText() + ".*", 4);
 			RowFilter<TableModelTabCustomer, Object> rfOrt = RowFilter.regexFilter(
 					"(?i)^.*" + txtSearch.getText() + ".*", 5);
-			
+
 			filters.add(rfID);
 			filters.add(rfSurname);
 			filters.add(rfName);
 			filters.add(rfStreet);
 			filters.add(rfPLZ);
 			filters.add(rfOrt);
-			
+
 			rf = RowFilter.orFilter(filters);
 		} catch (java.util.regex.PatternSyntaxException e) {
 			return;
@@ -254,45 +265,42 @@ public class TabCustomer extends JPanel implements Observer
 		sorter.setRowFilter(rf);
 	}
 
-    @Override
-    public void update(Observable o, Object arg)
-    {
-        updateFields();
-    }
-    
-    private void updateFields() {
-        updateStatistics();
-    }
-    
-    private void updateStatistics()
-    {
-        lblAmountOfCustomers.setText(String.valueOf(library.getCustomers().size()));
-    }
+	@Override
+	public void update(Observable o, Object arg) {
+		updateFields();
+	}
 
-    private void openCustomerEdit(Customer customerToEdit) {
-        if (openOnce.containsKey(customerToEdit)) {
-            openOnce.get(customerToEdit).setVisible(true);
-            openOnce.get(customerToEdit).toFront();
-        } else {
-            final Customer customerToRemove = customerToEdit;
-            CustomerEdit customerEdit = new CustomerEdit(customerToEdit, library);
-            customerEdit.addWindowListener(new WindowAdapter() {
-                public void windowClosing(WindowEvent e) {
-                    openOnce.remove(customerToRemove);
-                }
-            });
-            openOnce.put(customerToEdit, customerEdit);
-        }
+	private void updateFields() {
+		updateStatistics();
+	}
 
-    }
-    
-    private void openCustomerEditForAllSelectedCustomers(){
-        int selected[] = table.getSelectedRows();
-        for (int i : selected)
-        {
-            Customer c = tableModel.getCustomerAtRow(table.convertRowIndexToModel(i));
-            openCustomerEdit(c);
-        }
-    }
-    
+	private void updateStatistics() {
+		lblAmountOfCustomers.setText(String.valueOf(library.getCustomers().size()));
+	}
+
+	private void openCustomerEdit(Customer customerToEdit) {
+		if (openOnce.containsKey(customerToEdit)) {
+			openOnce.get(customerToEdit).setVisible(true);
+			openOnce.get(customerToEdit).toFront();
+		} else {
+			final Customer customerToRemove = customerToEdit;
+			CustomerEdit customerEdit = new CustomerEdit(customerToEdit, library);
+			customerEdit.addWindowListener(new WindowAdapter() {
+				public void windowClosing(WindowEvent e) {
+					openOnce.remove(customerToRemove);
+				}
+			});
+			openOnce.put(customerToEdit, customerEdit);
+		}
+
+	}
+
+	private void openCustomerEditForAllSelectedCustomers() {
+		int selected[] = table.getSelectedRows();
+		for (int i : selected) {
+			Customer c = tableModel.getCustomerAtRow(table.convertRowIndexToModel(i));
+			openCustomerEdit(c);
+		}
+	}
+
 }
